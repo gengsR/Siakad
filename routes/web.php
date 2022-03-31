4e5d95ea515a1ef37579582b977c97da865edb5a
gengsR <rahmaputru997@gmail.com>
2022-03-31 09:36:06 +0700

    Praktikum Jobbsheet 7

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('mahasiswa', MahasiswaController::class);
+
+
